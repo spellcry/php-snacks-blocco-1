64911c6dc18f92ace7a4f1a2ed7f724efaf4afbc
spellcry <sangiothebest@hotmail.com>
2022-09-21 14:27:48 +0200

    init

---
+++ php/snack-4.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 1</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <h1 class="title">Snack 1</h1>
+
+    <a href="../index.html">Home</a>
+</body>
+</html>
